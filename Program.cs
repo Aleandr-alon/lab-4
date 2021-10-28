9f6b9a7fc15b3e7bf028b27a9cadcac100bbd37b
Alexander Malyshenko <alexandr.mal.38@gmail.com>
2021-10-28 21:35:55 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] digits = new int[N];
+            for (int i = 0; i < digits.Length; i++)
+            {
+                digits[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int minDigit = Array.FindLast(digits, element => element < 0);
+            int index = Array.FindLastIndex(digits,element => element == minDigit);
+            Console.WriteLine(minDigit);
+            Console.WriteLine(index);
+            Console.ReadKey();
+
+        }
+    }
+}
+        
+    
